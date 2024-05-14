004cc246088fd0d36ef141dbd02d14b5eec6fcbb
Lucia <150481251+lucia8731@users.noreply.github.com>
2024-05-14 10:56:30 -0400

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -554,9 +554,9 @@ def calculate_total_tax(income, income_tax, resident_tax):
         total_tax = income_tax + resident_tax
 
     return {
-        'total_tax': total_tax,
-        'income_tax': income_tax,
-        'resident_tax': resident_tax,
+        'total_tax': "{:,.0f}".format(total_tax),
+        'income_tax': "{:,0f}".format(income_tax),
+        'resident_tax': "{:,.0f}".format(resident_tax),
     }
 
 if __name__ == '__main__':
